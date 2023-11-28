f320088200514382a613c6ef78c59fb300d2da0c
Felipe Acuña <felipe.acunago@gmail.com>
2023-11-28 15:31:12 -0300

    model-test works with implementations but with warnings

--- challenge/model.py
+++ challenge/model.py
@@ -1,6 +1,24 @@
 import pandas as pd
+from datetime import datetime
+import numpy as np
+
+from sklearn.linear_model import LogisticRegression
 
 from typing import Tuple, Union, List
+import joblib
+
+top_10_features = [
+    "OPERA_Latin American Wings", 
+    "MES_7",
+    "MES_10",
+    "OPERA_Grupo LATAM",
+    "MES_12",
+    "TIPOVUELO_I",
+    "MES_4",
+    "MES_11",
+    "OPERA_Sky Airline",
+    "OPERA_Copa Air"
+]
 
 class DelayModel:
 
@@ -9,11 +27,59 @@ class DelayModel:
     ):
         self._model = None # Model should be saved in this attribute.
 
+    def get_period_day(self, date):
+        date_time = datetime.strptime(date, '%Y-%m-%d %H:%M:%S').time()
+        morning_min = datetime.strptime("05:00", '%H:%M').time()
+        morning_max = datetime.strptime("11:59", '%H:%M').time()
+        afternoon_min = datetime.strptime("12:00", '%H:%M').time()
+        afternoon_max = datetime.strptime("18:59", '%H:%M').time()
+        evening_min = datetime.strptime("19:00", '%H:%M').time()
+        evening_max = datetime.strptime("23:59", '%H:%M').time()
+        night_min = datetime.strptime("00:00", '%H:%M').time()
+        night_max = datetime.strptime("4:59", '%H:%M').time()
+        
+        if(date_time > morning_min and date_time < morning_max):
+            return 'mañana'
+        elif(date_time > afternoon_min and date_time < afternoon_max):
+            return 'tarde'
+        elif(
+            (date_time > evening_min and date_time < evening_max) or
+            (date_time > night_min and date_time < night_max)
+        ):
+            return 'noche'
+        
+    def is_high_season(self, fecha):
+        fecha_año = int(fecha.split('-')[0])
+        fecha = datetime.strptime(fecha, '%Y-%m-%d %H:%M:%S')
+        range1_min = datetime.strptime('15-Dec', '%d-%b').replace(year = fecha_año)
+        range1_max = datetime.strptime('31-Dec', '%d-%b').replace(year = fecha_año)
+        range2_min = datetime.strptime('1-Jan', '%d-%b').replace(year = fecha_año)
+        range2_max = datetime.strptime('3-Mar', '%d-%b').replace(year = fecha_año)
+        range3_min = datetime.strptime('15-Jul', '%d-%b').replace(year = fecha_año)
+        range3_max = datetime.strptime('31-Jul', '%d-%b').replace(year = fecha_año)
+        range4_min = datetime.strptime('11-Sep', '%d-%b').replace(year = fecha_año)
+        range4_max = datetime.strptime('30-Sep', '%d-%b').replace(year = fecha_año)
+        
+        if ((fecha >= range1_min and fecha <= range1_max) or 
+            (fecha >= range2_min and fecha <= range2_max) or 
+            (fecha >= range3_min and fecha <= range3_max) or
+            (fecha >= range4_min and fecha <= range4_max)):
+            return 1
+        else:
+            return 0
+        
+    def get_min_diff(self, data):
+        fecha_o = datetime.strptime(data['Fecha-O'], '%Y-%m-%d %H:%M:%S')
+        fecha_i = datetime.strptime(data['Fecha-I'], '%Y-%m-%d %H:%M:%S')
+        min_diff = ((fecha_o - fecha_i).total_seconds())/60
+        return min_diff
+    
+
     def preprocess(
         self,
         data: pd.DataFrame,
         target_column: str = None
-    ) -> Union(Tuple[pd.DataFrame, pd.DataFrame], pd.DataFrame):
+    ) -> Union[Tuple[pd.DataFrame, pd.DataFrame], pd.DataFrame]:
         """
         Prepare raw data for training or predict.
 
@@ -26,7 +92,28 @@ class DelayModel:
             or
             pd.DataFrame: features.
         """
-        return
+        threshold_in_minutes = 15
+            
+        data['high_season'] = data['Fecha-I'].apply(self.is_high_season)
+        data['period_day'] = data['Fecha-I'].apply(self.get_period_day)
+        data['min_diff'] = data.apply(self.get_min_diff, axis = 1)
+        data['delay'] = np.where(data['min_diff'] > threshold_in_minutes, 1, 0)
+
+
+        #features = data.drop(columns=[target_column])
+        features = pd.concat([
+            pd.get_dummies(data['OPERA'], prefix = 'OPERA'),
+            pd.get_dummies(data['TIPOVUELO'], prefix = 'TIPOVUELO'), 
+            pd.get_dummies(data['MES'], prefix = 'MES')], 
+            axis = 1
+        )
+        features = features[top_10_features]
+
+        if target_column:
+            target = pd.DataFrame(data[target_column])
+            return (features, target)
+        else:
+            return features
 
     def fit(
         self,
@@ -40,7 +127,17 @@ class DelayModel:
             features (pd.DataFrame): preprocessed data.
             target (pd.DataFrame): target.
         """
-        return
+        
+        # Data balance
+        n_y0 = len(target[target['delay'] == 0])
+        n_y1 = len(target[target['delay'] == 1])
+        scale = n_y0/n_y1
+
+        self._model = LogisticRegression(class_weight={1: n_y0/len(target), 0: n_y1/len(target)})
+        self._model.fit(features, target)
+        joblib.dump(self._model, 'model.sav')
+
+        return None
 
     def predict(
         self,
@@ -55,4 +152,7 @@ class DelayModel:
         Returns:
             (List[int]): predicted targets.
         """
-        return
+        self._model = joblib.load('model.sav')
+        y_hat = self._model.predict(features) # numpy array
+
+        return y_hat.tolist()
